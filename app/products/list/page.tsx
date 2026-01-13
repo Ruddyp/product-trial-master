e4e38bccd3658566f8f176314ca3ba05ea99cffe
Ruddy <ruddy.pionneau@orange.fr>
2026-01-13 14:51:10 +0100

    Ajout des filtrages par recherche et par prix

--- app/products/list/page.tsx
+++ app/products/list/page.tsx
@@ -2,10 +2,16 @@
 import ProductCard from "@/components/product/ProductCard";
 import { FormFields, ProductDialog } from "@/components/product/ProductDialog";
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useCart } from "@/context/CartContext";
 import { mockProducts, Product } from "@/lib/mockData";
+import { ArrowDown01, ArrowDown10, Euro, Search } from "lucide-react";
 import { useState } from "react";
 import { toast } from "sonner";
+
+type PriceSort = "no_sort" | "higher" | "lower";
 
 export default function ProductsList() {
   const { handleDeleteToCart } = useCart();
@@ -14,6 +20,8 @@ export default function ProductsList() {
     undefined
   );
   const [dialogState, setDialogState] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [priceSort, setPriceSort] = useState<PriceSort>("no_sort");
 
   const openDialog = (product?: Product) => {
     setSelectedProduct(product);
@@ -57,15 +65,58 @@ export default function ProductsList() {
     setDialogState(false);
   }
 
+  const filteredProducts = products
+    .filter((product) =>
+      product.name.toLowerCase().includes(searchValue.toLowerCase())
+    )
+    .sort((a, b) => {
+      if (priceSort === "no_sort") return 0;
+      return priceSort === "lower" ? a.price - b.price : b.price - a.price;
+    });
+
   return (
     <div className="flex flex-col size-full justify-start items-center gap-8 p-4">
-      <h1 className="text-2xl font-bold">{`Liste des produits (${products.length})`}</h1>
+      <h1 className="text-2xl font-bold">{`Liste des produits (${filteredProducts.length})`}</h1>
       <Button
         className="bg-sky-900 hover:bg-sky-900/85"
         onClick={() => openDialog()}
       >
         Créer produit
       </Button>
+      <Card className="flex p-6 w-full max-w-md items-center justify-start rounded-xl shadow-sm gap-4">
+        <div className="flex flex-col gap-1">
+          <Label htmlFor="search" className="text-sm font-medium text-gray-700">
+            Rechercher un produit
+          </Label>
+          <div className="relative">
+            <Search className="absolute left-2 top-2" size={18} />
+            <Input
+              id="search"
+              type="search"
+              placeholder="Rechercher..."
+              value={searchValue}
+              onChange={(event) => setSearchValue(event.target.value)}
+              className="pl-10 w-full focus-visible:ring-2"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col gap-1">
+          <Label className="text-sm font-medium text-gray-700">
+            Trier par prix
+          </Label>
+          <div className="cursor-pointer w-fit">
+            {priceSort === "no_sort" && (
+              <Euro size={32} onClick={() => setPriceSort("lower")} />
+            )}
+            {priceSort === "lower" && (
+              <ArrowDown01 size={32} onClick={() => setPriceSort("higher")} />
+            )}
+            {priceSort === "higher" && (
+              <ArrowDown10 size={32} onClick={() => setPriceSort("no_sort")} />
+            )}
+          </div>
+        </div>
+      </Card>
       <ProductDialog
         open={dialogState}
         closeDialog={closeDialog}
@@ -73,7 +124,7 @@ export default function ProductsList() {
         onSave={handleSubmit}
       />
       <div className="flex flex-wrap w-full gap-4 justify-center items-center">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <ProductCard
             key={product.id}
             product={product}
